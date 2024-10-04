9087de7acd133e639c9718a75d23c2e969601954
smokke04 <donfenyvesi@gmail.com>
2024-10-04 12:35:28 +0200

    Mozgó UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.MenuPanel = new System.Windows.Forms.Panel();
-            this.MainPanel = new System.Windows.Forms.Panel();
-            this.SearchPanel = new System.Windows.Forms.Panel();
-            this.CustBtn = new System.Windows.Forms.Button();
-            this.PartsBtn = new System.Windows.Forms.Button();
             this.RepairBtn = new System.Windows.Forms.Button();
-            this.SearchTxtBox = new System.Windows.Forms.TextBox();
+            this.PartsBtn = new System.Windows.Forms.Button();
+            this.CustBtn = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.Panel();
             this.MainTxtBox = new System.Windows.Forms.TextBox();
+            this.SearchPanel = new System.Windows.Forms.Panel();
+            this.SearchTxtBox = new System.Windows.Forms.TextBox();
             this.MenuPanel.SuspendLayout();
             this.MainPanel.SuspendLayout();
             this.SearchPanel.SuspendLayout();
@@ -49,75 +49,75 @@
             this.MenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
             this.MenuPanel.Location = new System.Drawing.Point(0, 0);
             this.MenuPanel.Name = "MenuPanel";
-            this.MenuPanel.Size = new System.Drawing.Size(200, 561);
+            this.MenuPanel.Size = new System.Drawing.Size(173, 561);
             this.MenuPanel.TabIndex = 1;
-            // 
-            // MainPanel
-            // 
-            this.MainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.MainPanel.AutoSize = true;
-            this.MainPanel.Controls.Add(this.MainTxtBox);
-            this.MainPanel.Controls.Add(this.SearchPanel);
-            this.MainPanel.Location = new System.Drawing.Point(200, 0);
-            this.MainPanel.Name = "MainPanel";
-            this.MainPanel.Size = new System.Drawing.Size(734, 561);
-            this.MainPanel.TabIndex = 2;
-            // 
-            // SearchPanel
-            // 
-            this.SearchPanel.Controls.Add(this.SearchTxtBox);
-            this.SearchPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.SearchPanel.Location = new System.Drawing.Point(0, 0);
-            this.SearchPanel.Name = "SearchPanel";
-            this.SearchPanel.Size = new System.Drawing.Size(734, 100);
-            this.SearchPanel.TabIndex = 0;
-            // 
-            // CustBtn
-            // 
-            this.CustBtn.Location = new System.Drawing.Point(22, 128);
-            this.CustBtn.Name = "CustBtn";
-            this.CustBtn.Size = new System.Drawing.Size(160, 73);
-            this.CustBtn.TabIndex = 0;
-            this.CustBtn.Text = "Ügyfelek";
-            this.CustBtn.UseVisualStyleBackColor = true;
-            // 
-            // PartsBtn
-            // 
-            this.PartsBtn.Location = new System.Drawing.Point(22, 239);
-            this.PartsBtn.Name = "PartsBtn";
-            this.PartsBtn.Size = new System.Drawing.Size(160, 73);
-            this.PartsBtn.TabIndex = 1;
-            this.PartsBtn.Text = "Alkatrészek";
-            this.PartsBtn.UseVisualStyleBackColor = true;
             // 
             // RepairBtn
             // 
-            this.RepairBtn.Location = new System.Drawing.Point(22, 351);
+            this.RepairBtn.Location = new System.Drawing.Point(12, 344);
             this.RepairBtn.Name = "RepairBtn";
-            this.RepairBtn.Size = new System.Drawing.Size(160, 73);
+            this.RepairBtn.Size = new System.Drawing.Size(146, 50);
             this.RepairBtn.TabIndex = 2;
             this.RepairBtn.Text = "Szerelések";
             this.RepairBtn.UseVisualStyleBackColor = true;
+            this.RepairBtn.Click += new System.EventHandler(this.RepairBtn_Click);
             // 
-            // SearchTxtBox
+            // PartsBtn
             // 
-            this.SearchTxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.SearchTxtBox.Location = new System.Drawing.Point(11, 31);
-            this.SearchTxtBox.Name = "SearchTxtBox";
-            this.SearchTxtBox.Size = new System.Drawing.Size(711, 35);
-            this.SearchTxtBox.TabIndex = 0;
+            this.PartsBtn.Location = new System.Drawing.Point(12, 245);
+            this.PartsBtn.Name = "PartsBtn";
+            this.PartsBtn.Size = new System.Drawing.Size(146, 50);
+            this.PartsBtn.TabIndex = 1;
+            this.PartsBtn.Text = "Alkatrészek";
+            this.PartsBtn.UseVisualStyleBackColor = true;
+            this.PartsBtn.Click += new System.EventHandler(this.PartsBtn_Click);
+            // 
+            // CustBtn
+            // 
+            this.CustBtn.Location = new System.Drawing.Point(12, 147);
+            this.CustBtn.Name = "CustBtn";
+            this.CustBtn.Size = new System.Drawing.Size(146, 50);
+            this.CustBtn.TabIndex = 0;
+            this.CustBtn.Text = "Ügyfelek";
+            this.CustBtn.UseVisualStyleBackColor = true;
+            this.CustBtn.Click += new System.EventHandler(this.CustBtn_Click);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.MainPanel.Controls.Add(this.MainTxtBox);
+            this.MainPanel.Controls.Add(this.SearchPanel);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(173, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(761, 561);
+            this.MainPanel.TabIndex = 2;
             // 
             // MainTxtBox
             // 
-            this.MainTxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.MainTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.MainTxtBox.Location = new System.Drawing.Point(118, 147);
+            this.MainTxtBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.MainTxtBox.Location = new System.Drawing.Point(132, 147);
             this.MainTxtBox.Multiline = true;
             this.MainTxtBox.Name = "MainTxtBox";
             this.MainTxtBox.Size = new System.Drawing.Size(512, 367);
             this.MainTxtBox.TabIndex = 1;
+            // 
+            // SearchPanel
+            // 
+            this.SearchPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.SearchPanel.Controls.Add(this.SearchTxtBox);
+            this.SearchPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SearchPanel.Location = new System.Drawing.Point(0, 0);
+            this.SearchPanel.Name = "SearchPanel";
+            this.SearchPanel.Size = new System.Drawing.Size(761, 100);
+            this.SearchPanel.TabIndex = 0;
+            // 
+            // SearchTxtBox
+            // 
+            this.SearchTxtBox.Location = new System.Drawing.Point(11, 31);
+            this.SearchTxtBox.Name = "SearchTxtBox";
+            this.SearchTxtBox.Size = new System.Drawing.Size(738, 35);
+            this.SearchTxtBox.TabIndex = 0;
             // 
             // Main_Form
             // 
@@ -128,18 +128,19 @@
             this.Controls.Add(this.MainPanel);
             this.Controls.Add(this.MenuPanel);
             this.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.MinimumSize = new System.Drawing.Size(950, 600);
             this.Name = "Main_Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Autóműhley Karbantartás";
+            this.Load += new System.EventHandler(this.Main_Form_Load);
+            this.Resize += new System.EventHandler(this.Main_Form_Resize);
             this.MenuPanel.ResumeLayout(false);
             this.MainPanel.ResumeLayout(false);
             this.MainPanel.PerformLayout();
             this.SearchPanel.ResumeLayout(false);
             this.SearchPanel.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,72 @@ namespace AutoMuhely
 {
     public partial class Main_Form : Form
     {
+        private Rectangle custBtnOriginalRectangle;
+        private Rectangle partsBtnOriginalRectangle;
+        private Rectangle repairBtnOriginalRectangle;
+        private Rectangle menuPanelOriginalRectangle;
+        private Rectangle mainPanelOriginalRectangle;
+        private Rectangle searchPanelOriginalRectangle;
+        private Rectangle mainTxtBoxOriginalRectangle;
+        private Rectangle searchTxtBoxOriginalRectangle;
+
+        private Size originalFormSize;
         public Main_Form()
         {
             InitializeComponent();
+        }
+
+        private void Main_Form_Load(object sender, EventArgs e)
+        {
+            originalFormSize = this.Size;
+            custBtnOriginalRectangle = new Rectangle(CustBtn.Location.X, CustBtn.Location.Y, CustBtn.Width, CustBtn.Height);
+            partsBtnOriginalRectangle = new Rectangle(PartsBtn.Location.X, PartsBtn.Location.Y, PartsBtn.Width, PartsBtn.Height);
+            repairBtnOriginalRectangle = new Rectangle(RepairBtn.Location.X, RepairBtn.Location.Y, RepairBtn.Width, RepairBtn.Height);
+            menuPanelOriginalRectangle = new Rectangle(MenuPanel.Location.X, MenuPanel.Location.Y, MenuPanel.Width, MenuPanel.Height);
+            mainPanelOriginalRectangle= new Rectangle(MainPanel.Location.X, MainPanel.Location.Y, MainPanel.Width, MainPanel.Height);
+            searchPanelOriginalRectangle= new Rectangle(SearchPanel.Location.X, SearchPanel.Location.Y, SearchPanel.Width, SearchPanel.Height);
+            mainTxtBoxOriginalRectangle = new Rectangle(MainTxtBox.Location.X, MainTxtBox.Location.Y, MainTxtBox.Width, MainTxtBox.Height);
+            searchTxtBoxOriginalRectangle = new Rectangle(SearchTxtBox.Location.X, SearchTxtBox.Location.Y, SearchTxtBox.Width, SearchTxtBox.Height);
+        }
+        private void ResizeControl(Rectangle r, Control c)
+        {
+            float xRatio = (float)this.Width / originalFormSize.Width;
+            float yRatio = (float)this.Height / originalFormSize.Height;
+
+            int newX = (int)(r.X * xRatio);
+            int newY = (int)(r.Y * yRatio);
+            int newWidth = (int)(r.Width * xRatio);
+            int newHeight = (int)(r.Height * yRatio);
+
+            c.Location = new Point(newX, newY);
+            c.Size = new Size(newWidth, newHeight);
+        }
+
+        private void Main_Form_Resize(object sender, EventArgs e)
+        {
+            ResizeControl(custBtnOriginalRectangle, CustBtn);
+            ResizeControl(partsBtnOriginalRectangle, PartsBtn);
+            ResizeControl(repairBtnOriginalRectangle, RepairBtn);
+            ResizeControl(menuPanelOriginalRectangle, MenuPanel);
+            ResizeControl(mainPanelOriginalRectangle, MainPanel);
+            ResizeControl(searchPanelOriginalRectangle, SearchPanel);
+            ResizeControl(mainTxtBoxOriginalRectangle, MainTxtBox);
+            ResizeControl(searchTxtBoxOriginalRectangle, SearchTxtBox);
+        }
+
+        private void CustBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PartsBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RepairBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
